5ae6f88785575995c7ee5963580c76c69c930444
stav zilber <93096648+stavzilber21@users.noreply.github.com>
2021-12-22 19:01:38 +0200

    Add files via upload

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,3 +1,4 @@
+from asyncio import PriorityQueue
 from typing import List
 import json
 import os
@@ -42,30 +43,30 @@ class GraphAlgo(GraphAlgoInterface):
         except Exception:
             return False
 
+    def dijkstra(self, src) -> (list, list):
+        D = {v: float('inf') for v in range(self.graph.nodes)}
+        D[src] = 0
+
+        pq = PriorityQueue()
+        pq.put((0, src))
+
+        while not pq.empty():
+            (dist, current_vertex) = pq.get()
+            self.graph.visited.append(current_vertex)
+
+            for neighbor in range(self.graph.nodes):
+                if self.graph.edges[current_vertex][neighbor] != -1:
+                    distance = self.graph.edges[current_vertex][neighbor]
+                    if neighbor not in self.graph.visited:
+                        old_cost = D[neighbor]
+                        new_cost = D[current_vertex] + distance
+                        if new_cost < old_cost:
+                            pq.put((new_cost, neighbor))
+                            D[neighbor] = new_cost
+        return D
+
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        """
-        Returns the shortest path from node id1 to node id2 using Dijkstra's Algorithm
-        @param id1: The start node id
-        @param id2: The end node id
-        @return: The distance of the path, a list of the nodes ids that the path goes through
-        Example:
-#      >>> from GraphAlgo import GraphAlgo
-#       >>> g_algo = GraphAlgo()
-#        >>> g_algo.addNode(0)
-#        >>> g_algo.addNode(1)
-#        >>> g_algo.addNode(2)
-#        >>> g_algo.addEdge(0,1,1)
-#        >>> g_algo.addEdge(1,2,4)
-#        >>> g_algo.shortestPath(0,1)
-#        (1, [0, 1])
-#        >>> g_algo.shortestPath(0,2)
-#        (5, [0, 1, 2])
-        Notes:
-        If there is no path between id1 and id2, or one of them dose not exist the function returns (float('inf'),[])
-        More info:
-        https://en.wikipedia.org/wiki/Dijkstra's_algorithm
-        """
-        raise NotImplementedError
+
 
     def TSP(self, node_lst: List[int]) -> (List[int], float):
         """
